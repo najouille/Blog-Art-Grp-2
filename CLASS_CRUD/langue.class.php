6a28e1db9d7c432ef58b3f1e264e26a2f840c445
Mattèo Gauthier <matteo.gauthier@gmail.com>
2021-01-30 09:57:41 +0100

    Lang CRUD + main

--- CLASS_CRUD/langue.class.php
+++ CLASS_CRUD/langue.class.php
@@ -1,79 +1,99 @@
-<?php
-	// CRUD LANGUE (ETUD)
-
-	require_once __DIR__ . '../../CONNECT/database.php';
-
-	class LANGUE{
-		function get_1Langue($numLang){
-
-
+	<?php
+	require_once __DIR__ . "../../CONNECT/database.php";
+	class LANGUE
+	{
+		function get_1Langue($numLang)
+		{
+			global $db;
+			$queryText = "SELECT * FROM langue WHERE numLang = :numLang";
+			$query = $db->prepare($queryText);
+			$query->bindParam(":numLang", $numLang);
+			$query->execute();
+			$result = $query->fetch();
+			$query->closeCursor();
+			return $result;
 		}
-
-		function get_1LangueByPays($numLang){
-
-
+		function get_1LangueByPays($numLang)
+		{
+			global $db;
+			$queryText =
+				"SELECT * FROM langue INNER JOIN pays ON langue.numPays = pays.numPays WHERE numLang =:numLang";
+			$query = $db->prepare($queryText);
+			$query->bindParam(":numLang", $numLang);
+			$query->execute();
+			$result = $query->fetch();
+			$query->closeCursor();
+			return $result;
 		}
-
-		function get_AllLangues(){
-
-
+		function get_AllLangues()
+		{
+			global $db;
+			$queryText = "SELECT * FROM langue";
+			$query = $db->query($queryText);
+			$result = $query->fetchAll();
+			$query->closeCursor();
+			return $result;
 		}
-
-		function get_AllLanguesByPays(){
-
-
+		function get_AllLanguesByPays()
+		{
+			global $db;
+			$queryText =
+				"SELECT * FROM langue INNER JOIN pays ON langue.numPays = pays.numPays";
+			$query = $db->prepare($queryText);
+			$query->execute();
+			$result = $query->fetchAll();
+			$query->closeCursor();
+			return $result;
 		}
-
-		function create($numLang, $lib1Lang, $lib2Lang, $numPays){
-
+		function create($numLang, $lib1Lang, $lib2Lang, $numPays)
+		{
+			global $db;
 			try {
-          $db->beginTransaction();
-
-
-
-					$db->commit();
-					$request->closeCursor();
-			}
-			catch (PDOException $e) {
-					die('Erreur insert LANGUE : ' . $e->getMessage());
-					$db->rollBack();
-					$request->closeCursor();
+				$queryText =
+					"INSERT INTO langue (numLang, lib1Lang, lib2Lang, numPays) VALUES (?, ?, ?, ?)";
+				$db->beginTransaction();
+				$query = $db->prepare($queryText);
+				$query->execute([$numLang, $lib1Lang, $lib2Lang, $numPays]);
+				$db->commit();
+				$query->closeCursor();
+			} catch (PDOException $e) {
+				die("Erreur insert LANGUE : " . $e->getMessage());
+				$db->rollBack();
+				$query->closeCursor();
 			}
 		}
-
-		function update($numLang, $lib1Lang, $lib2Lang, $numPays){
-
+		function update($numLang, $lib1Lang, $lib2Lang, $numPays)
+		{
+			global $db;
 			try {
-          $db->beginTransaction();
-
-
-
-					$db->commit();
-					$request->closeCursor();
-			}
-			catch (PDOException $e) {
-					die('Erreur update LANGUE : ' . $e->getMessage());
-					$db->rollBack();
-					$request->closeCursor();
+				$db->beginTransaction();
+				$queryText =
+					"UPDATE langue SET lib1Lang = ?, lib2Lang = ?, numPays = ? WHERE numLang = ?";
+				$query = $db->prepare($queryText);
+				$query->execute([$lib1Lang, $lib2Lang, $numPays, $numLang]);
+				$db->commit();
+				$query->closeCursor();
+			} catch (PDOException $e) {
+				die("Erreur update LANGUE : " . $e->getMessage());
+				$db->rollBack();
+				$query->closeCursor();
 			}
 		}
-
-		// Ctrl FK sur THEMATIQUE, ANGLE, MOTCLE avec del
-		function delete($numLang){
-
+		function delete($numLang)
+		{
+			global $db;
 			try {
-          $db->beginTransaction();
-
-
-
-					$db->commit();
-					$request->closeCursor();
-
-			}
-			catch (PDOException $e) {
-					die('Erreur delete LANGUE : ' . $e->getMessage());
-					$db->rollBack();
-					$request->closeCursor();
+				$queryText = "DELETE FROM langue WHERE numLang = :numLang";
+				$db->beginTransaction();
+				$query = $db->prepare($queryText);
+				$query->bindParam(":numLang", $numLang);
+				$query->execute();
+				$db->commit();
+				$query->closeCursor();
+			} catch (PDOException $e) {
+				die("Erreur delete LANGUE : " . $e->getMessage());
+				$db->rollBack();
+				$query->closeCursor();
 			}
 		}
-	}	// End of class
+	} // End of class
--- back/langue/langue.php
+++ back/langue/langue.php
@@ -9,10 +9,14 @@
 
 // Mode DEV
 require_once __DIR__ . '/../../util/utilErrOn.php';
+require_once __DIR__ . '/../../CLASS_CRUD/langue.class.php';
+global $db;
+$statut = new LANGUE;
 
 ?>
 <!DOCTYPE html>
 <html lang="fr">
+
 <head>
     <meta charset="utf-8" />
     <title>Admin - Gestion du CRUD Langue</title>
@@ -22,17 +26,60 @@ require_once __DIR__ . '/../../util/utilErrOn.php';
 
     <link href="../css/style.css" rel="stylesheet" type="text/css" />
 </head>
+
 <body>
     <h1>BLOGART21 Admin - Gestion du CRUD Langue</h1>
 
+    <h2>Tous les Langues</h2>
+    <hr /><br />
+    <h2>Nouvelle Langue :&nbsp;<a href="./createLangue.php"><i>Créer une langue</i></a></h2>
+    <br />
+    <hr />
+    <h2>Tous les statuts</h2>
+
+    
+
+    <table border="3" bgcolor="aliceblue">
+        <thead>
+            <tr>
+                <th>&nbsp;NumLang&nbsp;</th>
+                <th>&nbsp;Lib1&nbsp;</th>
+                <th>&nbsp;Lib2&nbsp;</th>
+                <th>&nbsp;NumPays&nbsp;</th>
+                <th colspan="2">&nbsp;Action&nbsp;</th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php
+            $allLang = $statut->get_AllLangues();
+            foreach ($allLang as $row) {
+            ?>
+                <tr>
+                    <td>
+                        <h4>&nbsp; <?php echo $row["numLang"]; ?> &nbsp;</h4>
+                    </td>
+                    <td>&nbsp; <?php echo $row["lib1Lang"]; ?> &nbsp;</td>
+                    <td>&nbsp; <?php echo $row["lib2Lang"]; ?> &nbsp;</td>
+                    <td>&nbsp; <?php echo $row["numPays"]; ?> &nbsp;</td>
+
+                    <td>&nbsp;<a href="./updateLangue.php?id=<?= $row["numLang"]; ?>"><i>Modifier</i></a>&nbsp;
+                        <br />
+                    </td>
+                    <td>&nbsp;<a href="./deleteLangue.php?id=<?= $row["numLang"]; ?>"><i>Supprimer</i></a>&nbsp;
+                        <br />
+                    </td>
+                </tr>
+            <?php
+            }    // End of foreach
+            ?>
+        </tbody>
+    </table>
+
     <br><br>
 
-    <h2>En construction :-)</h2>
-
-    <br><br>
-
-<?php
+    <?
 require_once __DIR__ . '/footer.php';
 ?>
 </body>
+
 </html>
