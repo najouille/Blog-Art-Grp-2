b8f0e39c6fa50be353b13e56bd1dded311d453e8
Mattèo Gauthier <matteo.gauthier@gmail.com>
2021-02-12 14:42:17 +0100

    Add new css file + delete useless

--- hello.php
+++ hello.php
@@ -11,6 +11,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <link rel="stylesheet" href="./front/assets/master.css">
+    <link rel="stylesheet" href="./front/assets/global.css">
 </head>
 
 <body>
