8d24e0314ba6e08041d580734864254d26b022b3
Mattèo Gauthier <matteo.gauthier@gmail.com>
2021-02-23 16:39:20 +0100

    Fix blog.php issue with minireset.css

--- blog/index.php
+++ blog/index.php
@@ -2,9 +2,9 @@
 <html lang="en">
 
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <?php
+    include('../front/includes/commons/headRequired.php')
+    ?>
     <title>Document</title>
     <link rel="stylesheet" href="../front/assets/master.css">
     <link rel="stylesheet" href="../front/assets/blog.css">
