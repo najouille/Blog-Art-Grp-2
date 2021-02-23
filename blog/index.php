40654cb1152fb099fff2d414a523903169cffa62
Candice Fradet <onc.fradet@gmail.com>
2021-02-23 11:47:57 +0100

    création de la page galerie + composent navbar

--- blog/index.php
+++ blog/index.php
@@ -1,62 +1,9 @@
-<?php
-
-?>
-<div class="header">
-    <img class="logo-navbar" src="../front/assets/image/Townyart-blanc (2).svg" alt="">
-    <div class="navbar-content-right">
-        <a href="#" class="bouton-space">
-            ACCUEIL
-        </a>
-        <a href="#" class="bouton-space">
-            BLOG
-        </a>
-        <a href="#" class="bouton-space">
-            GALLERIE
-        </a>
-        <a href="#" class="bouton-space">
-            CONTACT
-        </a>
-        <a href="#" class="icon-right bouton-space">
-
-            <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="#000000" viewBox="0 0 256 256">
-                <rect width="256" height="256" fill="none"></rect>
-                <circle cx="128" cy="128" r="96" fill="none" stroke="currentColor" stroke-miterlimit="10" stroke-width="16"></circle>
-                <circle cx="128" cy="120" r="40" fill="none" stroke="currentColor" stroke-miterlimit="10" stroke-width="16"></circle>
-                <path d="M63.79905,199.37405a72.02812,72.02812,0,0,1,128.40177-.00026" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="16"></path>
-            </svg>
-        </a>
-    </div>
-
-</div>
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-<link rel="preconnect" href="https://fonts.gstatic.com">
-<link href="https://fonts.googleapis.com/css2?family=Manrope:wght@300;400;500;600;700&display=swap" rel="stylesheet">
 <!DOCTYPE html>
 <html lang="en">
 
 <head>
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Manrope:wght@300;400;500;600;700&display=swap" rel="stylesheet">
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -66,6 +13,9 @@
 </head>
 
 <body class='style'>
+    <?php
+    include('../front\includes\commons\navbar.php')
+    ?>
     <div class='maxiconteneurblog'>
         <div class='etiquetteblog'>
             <img class='tailleimg' src="../front/assets/image/jon-tyson-HuRTqaEMD4I-unsplash 1.png" alt="street art jazz">
