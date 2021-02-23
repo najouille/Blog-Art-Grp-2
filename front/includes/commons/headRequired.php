8597b14029bea371d9b16824219e1410aff94196
Mattèo Gauthier <matteo.gauthier@gmail.com>
2021-02-23 15:16:42 +0100

    Adding Monument Extended font on the project

--- front/includes/commons/headRequired.php
+++ front/includes/commons/headRequired.php
@@ -2,6 +2,6 @@
 <meta http-equiv="X-UA-Compatible" content="IE=edge" />
 <meta name="viewport" content="width=device-width, initial-scale=1.0" />
 <link rel="preconnect" href="https://fonts.gstatic.com" />
-<link href="https://fonts.googleapis.com/css2?family=Manrope:wght@300;400;500;600;700&display=swap" rel="stylesheet">
-<link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/jgthms/minireset.css@master/minireset.min.css">
-<!-- <link rel="stylesheet" href="./front/assets/master.css" /> -->
+<link href="https://fonts.googleapis.com/css2?family=Manrope:wght@300;400;500;600;700&display=swap" rel="stylesheet"/>
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/jgthms/minireset.css@master/minireset.min.css"/>
+<!-- <link rel="stylesheet" href="./front/assets/master.css" /> --> 
