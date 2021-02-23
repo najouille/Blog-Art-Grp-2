cf7e9ae0ab1642a45106dfc6b0409a36e96b7e0d
Candice Fradet <onc.fradet@gmail.com>
2021-02-23 16:02:54 +0100

    Gallerie, fix blog

--- blog/index.php
+++ blog/index.php
@@ -2,17 +2,15 @@
 <html lang="en">
 
 <head>
-    <?php
-    include('../front/includes/commons/headRequired.php')
-    ?>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="../front/assets/master.css">
     <link rel="stylesheet" href="../front/assets/blog.css">
 </head>
 
 <body class='style'>
-    <?php
-    include('../front\includes\commons\navbar.php')
-    ?>
     <div class='maxiconteneurblog'>
         <div class='etiquetteblog'>
             <img class='tailleimg' src="../front/assets/image/jon-tyson-HuRTqaEMD4I-unsplash 1.png" alt="street art jazz">
