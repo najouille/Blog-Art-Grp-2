9771a8d25e494ac0e08dc9c2631a98a71a9da282
Vanon Borget <vanonborget@MacBook-Air-de-Vanon.local>
2021-02-23 11:50:08 +0100

    style back motcle

--- back/motCle/updateMotCle.php
+++ back/motCle/updateMotCle.php
@@ -23,13 +23,12 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $libMotCle = $_POST["libMotCle"];
     $numLang = $_POST["numLang"];
 
-    
+
     if (isset($numMotCle) && isset($libMotCle) && isset($numLang)) {
         $motcle->update($numMotCle, $libMotCle, $numLang);
         $updated = true;
     } else {
         $updated = false;
-
     }
 } else {
     $numMotCle = $_GET["id"];
@@ -52,11 +51,15 @@ if ($resultMotCle) {
     <title>Admin - Gestion du CRUD Langue</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <link href="../css/style.css" rel="stylesheet" type="text/css" />
+    <link href="../css/back-office.css" rel="stylesheet" type="text/css" />
 </head>
 
-<body>
-    <h1>BLOGART21 Admin - Gestion du CRUD MotCle</h1>
-    <h2>Modifification d'un mot cle</h2>
+<body class="twa-back">
+    <div class="title">
+        <img class='logo' src="../../front/assets/image/Townyart.png" alt="logo-townyart">
+        <h1>BLOGART21 Admin - Gestion du CRUD MotClé</h1>
+    </div>
+    <h3>Modification d'un mot clé</h2>
 
     <?php
     if ($updated) {
@@ -68,43 +71,44 @@ if ($resultMotCle) {
 
 
     <form method="post" action="<?= "./updateMotCle.php?id=" . $numMotCle; ?>" enctype="multipart/form-data">
+        <div id="fieldset-container">
+            <fieldset>
+                <legend class="legend1">Formulaire Langue...</legend>
 
-        <fieldset>
-            <legend class="legend1">Formulaire Langue...</legend>
+                <input type="hidden" id="id" name="id" value="<?= $_GET['id']; ?>" />
 
-            <input type="hidden" id="id" name="id" value="<?= $_GET['id']; ?>" />
+                <div class="control-group">
+                    <label for="libMotCle">Libellé : </label>
+                    <input type="text" name="libMotCle" id="libMotCle" placeholder="Désignation" value="<?= $libMotCle ?>" autofocus>
 
-            <div class="control-group">
-                <label for="libMotCle">Libellé</label>
-                <input type="text" name="libMotCle" id="libMotCle" placeholder="Désignation" value="<?= $libMotCle ?>" autofocus>
-
-                <label class="control-label" for="numLang"><b>Pays :</b></label>
-                <select name="numLang" id="numLang">
-                    <?php
-                    $allLang = $lang->get_AllLangues();
-                    foreach ($allLang as $row) {
-                        if ($row["numLang"] === $numLang) {
-                            $selected = "selected";
-                        } else {
-                            $selected = "";
+                    <label class="control-label" for="numLang"><b>Pays :</b></label>
+                    <select name="numLang" id="numLang">
+                        <?php
+                        $allLang = $lang->get_AllLangues();
+                        foreach ($allLang as $row) {
+                            if ($row["numLang"] === $numLang) {
+                                $selected = "selected";
+                            } else {
+                                $selected = "";
+                            }
+                            echo '<option value="' . $row["numLang"] . '" ' . $selected . '>' . $row["lib2Lang"] . '</option>';
                         }
-                        echo '<option value="' . $row["numLang"] . '" ' . $selected . '>' . $row["lib2Lang"] . '</option>';
-                    }
-                    ?>
-                </select>
-            </div>
-
-            <div class="control-group">
-                <div class="controls">
-                    <br><br>
-                    
-                    <input type="submit" value="Initialiser" style="cursor:pointer; padding:5px 20px; background-color:lightsteelblue; border:dotted 2px grey; border-radius:5px;" name="Submit" />
-                    
-                    <input type="submit" value="Valider" style="cursor:pointer; padding:5px 20px; background-color:lightsteelblue; border:dotted 2px grey; border-radius:5px;" name="Submit" />
-                    <br>
+                        ?>
+                    </select>
                 </div>
-            </div>
-        </fieldset>
+
+                <div class="control-group">
+                    <div class="controls">
+                        <br><br>
+
+                        <input type="submit" value="Initialiser" name="Submit" />
+
+                        <input type="submit" value="Valider" name="Submit" />
+                        <br>
+                    </div>
+                </div>
+            </fieldset>
+        </div>
     </form>
     <?php
 
