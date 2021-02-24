a38126b11c447cd4935d9bdb4789c1d9bcd3b72c
Candice Fradet <onc.fradet@gmail.com>
2021-02-24 19:14:53 +0100

    finalisation page evenement article + start composant commentaire

---
+++ front/includes/commons/commentaire.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <?php include('../front/includes/commons/headRequired.php') ?>
+    <link rel="stylesheet" href="../front/assets/commentaire.css">
+</head>
+
+<body class='style'>
+
+
+
+
+
+</body>
+
+</html>
