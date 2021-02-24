fa2fade904a765d1d1232268f9cd6bcc1c0d2d99
Mattèo Gauthier <matteo.gauthier@gmail.com>
2021-02-24 22:31:56 +0100

    Fix admin.php style bug

--- admin.php
+++ admin.php
@@ -30,33 +30,33 @@ require_once __DIR__ . '/util/utilErrOn.php';
 		<h1>Panneau d'Admin : Gestion des CRUD - BLOGART21</h1>
 	</div>
 
-	<div >
-		
-		<a id="button-admin" class="input-button" href="./BACK/angle/angle.php">Angle </a>
-		
-		<a id="button-admin" class="input-button" href="./BACK/article/article.php">Article </a>
-		
-		<a id="button-admin" class="input-button" href="./BACK/comment/comment.php">Commentaire </a>
-		
-		<a id="button-admin" class="input-button" href="./BACK/commentplus/commentplus.php">Réponse sur Commentaire </a>
-		
-		<a id="button-admin" class="input-button" href="./BACK/langue/langue.php">Langue</a>
-		
-		<a id="button-admin" class="input-button" href="./BACK/likeart/likeart.php">Like Article </a>
-		
-		<a id="button-admin" class="input-button" href="./BACK/likecom/likecom.php">Like Commentaire </a>
-		
-		<a id="button-admin" class="input-button" href="./BACK/membre/membre.php">Membre </a>
-		
-		<a id="button-admin" class="input-button" href="./BACK/motcle/motcle.php">Mot-clé </a>
-		
-		<a id="button-admin" class="input-button" href="./BACK/motclearticle/motclearticle.php">Mot-clé Article </a>
-		
-		<a id="button-admin" class="input-button" href="./BACK/statut/statut.php">Statut (*)</a>
-		
-		<a id="button-admin" class="input-button" href="./BACK/thematique/thematique.php">Thématique </a>
-		
-		<a id="button-admin" class="input-button" href="./BACK/user/user.php">User </a>
+	<div class="admin-container">
+
+		<a class="button-admin input-button" href="./BACK/angle/angle.php">Angle </a>
+
+		<a class="button-admin input-button" href="./BACK/article/article.php">Article </a>
+
+		<a class="button-admin input-button" href="./BACK/comment/comment.php">Commentaire </a>
+
+		<a class="button-admin input-button" href="./BACK/commentplus/commentplus.php">Réponse sur Commentaire </a>
+
+		<a class="button-admin input-button" href="./BACK/langue/langue.php">Langue</a>
+
+		<a class="button-admin input-button" href="./BACK/likeart/likeart.php">Like Article </a>
+
+		<a class="button-admin input-button" href="./BACK/likecom/likecom.php">Like Commentaire </a>
+
+		<a class="button-admin input-button" href="./BACK/membre/membre.php">Membre </a>
+
+		<a class="button-admin input-button" href="./BACK/motcle/motcle.php">Mot-clé </a>
+
+		<a class="button-admin input-button" href="./BACK/motclearticle/motclearticle.php">Mot-clé Article </a>
+
+		<a class="button-admin input-button" href="./BACK/statut/statut.php">Statut (*)</a>
+
+		<a class="button-admin input-button" href="./BACK/thematique/thematique.php">Thématique </a>
+
+		<a class="button-admin input-button" href="./BACK/user/user.php">User </a>
 	</div>
 
 </body>
