a30ca4656b5b43e9fda6e860aebd69d97d686a24
Alexdup1 <alex-dupin@hotmail.fr>
2021-02-25 11:49:05 +0100

    insolites50%

---
+++ insolites.php
@@ -0,0 +1,87 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <?php include('./front/includes/commons/headRequired.php') ?>
+    <title>Document</title>
+    <link rel="stylesheet" href="./front/assets/insolites.css" />
+</head>
+
+<body>
+<?php
+    include('./front/includes/commons/navbar.php')
+    ?>
+
+<img src="front/assets/image/bordeaux.png" alt="ville"/>
+
+<h1>TOP 3 <br> des oeuvres les plus insolites de Bordeaux</h1>
+
+<section class="intro">
+<p>Bordeaux, de son surnom “La Belle endormie” du fait de sa quiétude, est en réalité un paradis pour les fans de street art. Du panneau de signalisation modifié aux magnifiques fresques murales en passant par des sculptures originales, Bordeaux regorge de street art à chaque coin de rue. On te propose dans cet article un top 3 des lieux insolites que tu dois absolument connaitre</p>
+</section>
+
+<div class="tags">
+        <h5>Insolite</h5>
+        <h5>Photographie</h5>
+        <h5>Street-art</h5>
+        <h5>Art</h5>
+
+<section class="top1">
+    <div id="top1">
+    <img src="front/assets/image/icones/number1.svg" alt="fresque"/></body>
+        </div>
+    <h2>La Jaguar du parking Victor-Hugo</h2>
+        <div id="img1">
+        <img src="front/assets/image/jaguar.png" alt="jaguar"/>
+        </div>
+    <p>Si tu es déjà venu à Bordeaux, tu es forcément passé par la rue St Catherine, mais tu n'es peut-être jamais sorti des sentiers battus. Pour les aventuriers, ceux qui, poussés par la curiosité décident de s'égarer dans les rues de Bordeaux, le parking Victor Hugo leur est familier. A vrai dire, il n'est pas vraiment caché puisqu'il se situe à 30 mètres de la promenade St Catherine. Mais qu'y a t'il de si spécial pour que l'on te parle de ce parking ? Ne t'en fais pas, on va te l'expliquer. <br>
+    Ce parking a été construit au début des années 60 par les architectes toulousains Jean Dauriac et Pierre Laffitte. Mais en 1993, lors de la rénovation de l’édifice, l’architecte bordelais Jean-François Dosso a eu l’idée de créer un signe distinctif pour ce parking. Il positionne une Jaguar MK1 verte encastrée dans la façade. <br>
+    A l’œil nu, le véhicule paraît suspendu au-dessus de nos têtes et ne tenant qu’à un fil de subir les lois de la gravité. Au premier regard, l'impression qu'un accident vient de se produire nous rend inquiet. C'est en se rapprochant de la scène et en réfléchissant quelques instants que l'on comprend la supercherie. En effet, la Jaguar est, en réalité, posée sur la structure même du parking.
+    En regardant depuis le sol, tu pourras constater que le moteur a été retiré de la carrosserie afin d’éviter de l’alourdir et pour conforter sa dimension artistique. <br>
+    Une œuvre insolite aux airs de films hollywoodiens qui ravira les passionnés d'automobiles comme les passants. Effet de surprise garanti à la place de la Ferme Richemont, 33000 Bordeaux. <br> </p>
+    </div>
+</section>
+
+<section class="top2">
+    <div id="top2">
+    <img src="front/assets/image/icones/number2.svg" alt="fresque"/></body>
+    <h2>La sculpture “Un détail”</h2>
+        <div id="img2">
+        <img src="front/assets/image/statue_portrait.png" alt="statue"/>
+        </div>
+    <p>          Alors que nous nous promenons sur les quais de la rive gauche, non loin de la gare Saint Jean, notre œil ne peut qu'être attiré par cette immense structure à l'architecture futuriste. Mais attention ! Si tu veux en voir la totalité, il te faudra lever la tête. <br>
+    Siégeant sur la rive gauche de la Garonne et située à cinq minutes à pied de la gare Saint-Jean, La MÉCA est une arche en béton brute mesurant 37 mètres de haut et 180 mètres de large. <br>
+    Conçu par l'architecte BIG (Bjarke Ingels Group) et conçu en septembre 2018, ce bâtiment est caractérisé par ses formes futuristes et ses dimensions gigantesques, comme si cette arche venait tout droit du futur. <br>
+    Derrière cet édifice se cache une statue étrangement perturbante reposant sur les gradins extérieurs. Cette œuvre, qui s’intitule “Un détail” est celle de Benoît Maire, artiste Pessacais de 43 ans. <br>
+    Faite de bronze, elle représente une demi-tête d'Hermès. Cette sculpture de 3 mètres de hauteur, à été réalisée par l’atelier de fonderie artisanale “Fonderie des cyclopes” à Mérignac. <br>
+    Selon l'artiste : « La moitié absente de la sculpture est à compléter par le spectateur dans l'intérieur vidé de la chambre urbaine. Ce travail de l'imagination est renforcé par le fini poli-miroir de la tranche coupée qui permet de faire l'expérience de la réflexion dans son sens littéral. » <br>
+    Une œuvre qui vaut le détour si tu aimes les illusions d’optiques et la sculpture, mais aussi la démesure. La MÉCA est situé au PARVIS CORTO MALTESE, Quai de Paludate, 33800 Bordeaux.</p> <br>
+    </div>
+</section>
+
+<section class="top3">
+    <div id="top3">
+    <img src="front/assets/image/icones/number3.svg" alt="fresque"/></body>
+    <h2>La fresque géante du CHU Pellegrin</h2>
+        <div id="img3">
+        <img src="front/assets/image/fresque.png" alt="fresque"/></body>
+        </div>
+        <div id="top3">
+        </div>
+    <p>          C’est en allant prendre mon repas au restaurant universitaire que je passe pour la première fois devant cet hôpital. Aux premiers abords, rien d'étonnant, jusqu’à ce que je me tourne vers la gauche. Là, j'aperçois une petite fille qui me fixe du regard. Une petite fille… de 15 mètres de hauteur. On est d'accord, c’est impossible… Mais tu vas bientôt comprendre pourquoi ça l'est. <br>
+    Remontons dans le temps jusqu’en 2013, année où l’artiste pochoiriste Jef Aerosol est appelé pour la réalisation d’une œuvre de 15m x 6m à l’entrée de l'hôpital Pellegrin. Cette œuvre, réalisée en 4 jours, représente une image forte et symbolique. En effet, cette fresque monumentale permet de casser l’image anxiogène qui règne à l’hôpital. <br>
+    Philippe Vigouroux, directeur général du CHU de Bordeaux en parle : 
+    “La petite fille tient avec précaution un globe terrestre, porteur de toute une humanité. Son sourire est celui de l’hospitalité, sa gravité est celle de la responsabilité de qui tient des vies entre ses mains. » <br>
+    Mais pourquoi une petite fille ? L’artiste nous confie que l'enfance, c'est l'innocence, l'avenir, l'espoir. La jeunesse, c'est à la fois le futur et aussi ce que nous laissons derrière nous. A travers cette fresque, cette petite fille nous dit que ce monde nous appartient et c’est à nous de savoir ce que nous voulons en faire. <br>
+    On te conseille vivement d’aller à l'hôpital Pellegrin… Non pas pour des soucis de santé on l’espère, mais pour admirer ce chef d'œuvre visible Rue de la Pelouse de Douet à Bordeaux. <br>
+    </p>
+    </div>
+</section>
+
+    
+
+    
+
+</html>
+
+</html
