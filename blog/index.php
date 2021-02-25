4f2a343a48b59bde9bb088bd85235590e5fbe541
Clément Calles <clement.calles@outlook.fr>
2021-02-25 12:31:43 +0100

    trier par

--- blog/index.php
+++ blog/index.php
@@ -30,6 +30,9 @@
             <circle cx="10" cy="10" r="7" />
             <line x1="21" y1="21" x2="15" y2="15" />
         </svg>
+        <span class="trier">
+            Trier par :
+        </span>
         </div>
     </div>
     <div class='maxiconteneurblog'>
