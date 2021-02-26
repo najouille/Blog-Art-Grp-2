ade960770149fe0ee057710b8525b4c56a91f016
Clément Calles <clement.calles@outlook.fr>
2021-02-26 11:11:36 +0100

    links page home et blog

--- blog/index.php
+++ blog/index.php
@@ -37,68 +37,68 @@
         </div>
     </div>
     <div class='maxiconteneurblog'>
-        <div class='etiquetteblog'>
+        <a href="" class='etiquetteblog'>
             <img class='tailleimg' src="<?= '../front/assets/image/jon-tyson-HuRTqaEMD4I-unsplash 1.png' ?>" alt="street art jazz">
             <div class='conteneurblog'>
                 <span class='tag'>
-                    Street-Art
-                </span>
-                <span class='tag'>
-                    Photographie
-                </span>
-                <span class='tag'>
-                    Evènement
-                </span>
-                <span class='tag'>
-                    Art
-                </span>
-            </div>
-            <h3>Le street-art à Bordeaux, un milieu des plus tendances</h3>
-            <h4>Vous apprendrez ici beaucoup sur l’origine du street-art, son application à Bordeaux ainsi que les nombreux évennements organisés autour de cet art.</h4>
-        </div>
-
-
-        <div class='etiquetteblog'>
-            <img class='tailleimg' src="<?= '../front/assets/image/portaitphotographe.png' ?>" alt="portrait">
-            <div class='conteneurblog'>
-                <span class='tag'>
-                    Urbain
-                </span>
-                <span class='tag'>
-                    Photographie
-                </span>
-                <span class='tag'>
-                    Architecture
-                </span>
-                <span class='tag'>
-                    Art
-                </span>
-            </div>
-            <h3>Portrait de Frédéric Ducos, photographe bordelais</h3>
-            <h4>Vous découvrirez, dans cet article, le parcours d’un photographe parti de rien qui, aujourd’hui, cotoie les plus grands.</h4>
-        </div>
-
-
-
-        <div class='etiquetteblog'>
-            <img class='tailleimg' src="<?= '../front/assets/image/Statue.png' ?>" alt="Statue">
-            <div class='conteneurblog'>
-                <span class='tag'>
-                    Street-Art
+                    street-art
                 </span>
                 <span class='tag'>
                     photographie
                 </span>
                 <span class='tag'>
-                    Insolite
+                    evènement
                 </span>
                 <span class='tag'>
-                    Art
+                    art
+                </span>
+            </div>
+            <h3>Le street-art à Bordeaux, un milieu des plus tendances</h3>
+            <h4>Vous apprendrez ici beaucoup sur l’origine du street-art, son application à Bordeaux ainsi que les nombreux évennements organisés autour de cet art.</h4>
+        </a>
+
+
+        <a href="../portrait.html" class='etiquetteblog'>
+            <img class='tailleimg' src="<?= '../front/assets/image/portaitphotographe.png' ?>" alt="portrait">
+            <div class='conteneurblog'>
+                <span class='tag'>
+                    urbain
+                </span>
+                <span class='tag'>
+                    photographie
+                </span>
+                <span class='tag'>
+                    architecture
+                </span>
+                <span class='tag'>
+                    art
+                </span>
+            </div>
+            <h3>Portrait de Frédéric Ducos, photographe bordelais</h3>
+            <h4>Vous découvrirez, dans cet article, le parcours d’un photographe parti de rien qui, aujourd’hui, cotoie les plus grands.</h4>
+        </a>
+
+
+
+        <a href="../insolites.php" class='etiquetteblog'>
+            <img class='tailleimg' src="<?= '../front/assets/image/Statue.png' ?>" alt="Statue">
+            <div class='conteneurblog'>
+                <span class='tag'>
+                    street-art
+                </span>
+                <span class='tag'>
+                    photographie
+                </span>
+                <span class='tag'>
+                    insolite
+                </span>
+                <span class='tag'>
+                    art
                 </span>
             </div>
             <h3>3 lieux insolites à découvrir absolument</h3>
             <h4>Bordeaux regorge de trésors cachés. Nous vous livrons dans cet article trois de ces derniers.</h4>
-        </div>
+        </a>
     </div>
     <div class='maxiconteneurblog opacity-10 soon-banner-container'>
         <div class="soon-banner">
@@ -109,16 +109,16 @@
             <img class='tailleimg' src="<?= '../front/assets/image/jon-tyson-HuRTqaEMD4I-unsplash 1.png' ?>" alt="street art jazz">
             <div class='conteneurblog'>
                 <span class='tag'>
-                    Street-Art
+                    street-art
                 </span>
                 <span class='tag'>
                     photographie
                 </span>
                 <span class='tag'>
-                    Evènement
+                    evènement
                 </span>
                 <span class='tag'>
-                    Art
+                    art
                 </span>
             </div>
             <h3>Le street-art à Bordeaux, un milieu des plus tendances</h3>
@@ -130,16 +130,16 @@
             <img class='tailleimg' src="<?= '../front/assets/image/portaitphotographe.png' ?>" alt="portrait">
             <div class='conteneurblog'>
                 <span class='tag'>
-                    Urbain
+                    urbain
                 </span>
                 <span class='tag'>
-                    Photographie
+                    photographie
                 </span>
                 <span class='tag'>
-                    Architecture
+                    architecture
                 </span>
                 <span class='tag'>
-                    Art
+                    art
                 </span>
             </div>
             <h3>Portrait de Frédéric Ducos, photographe bordelais</h3>
@@ -152,16 +152,16 @@
             <img class='tailleimg' src="<?php '../front/assets/image/Statue.png' ?>" alt="Statue">
             <div class='conteneurblog'>
                 <span class='tag'>
-                    Street-Art
+                    street-art
                 </span>
                 <span class='tag'>
                     photographie
                 </span>
                 <span class='tag'>
-                    Insolite
+                    insolite
                 </span>
                 <span class='tag'>
-                    Art
+                    art
                 </span>
             </div>
             <h3>3 lieux insolites à découvrir absolument</h3>
