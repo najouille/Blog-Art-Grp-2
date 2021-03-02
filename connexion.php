62d9f29f6dde710329b1751a58d6f7d82e798571
najouille <najadalmagne@gmail.com>
2021-03-02 17:49:10 +0100

    page contact : fix + required forms

--- connexion.php
+++ connexion.php
@@ -21,16 +21,14 @@
             <div>
                 <form>
                     <div class="e-mail connection">
-                        <input type="text" name="e-mail" id="e-mail" placeholder="E-mail">
+                        <input class="input-profil1" type="email" name="e-mail" id="e-mail" placeholder="Votre E-mail ..." required>
 
-                        <input type="text" name="mdp" id="amdp" placeholder="Mot de passe">
+                        <input class="input-profil1" type="password" name="mdp" id="amdp" minlength="8" placeholder="Votre mot de passe ..." required>
 
-                        <button name="button">Se connecter</button>
+                        <a><button class="button-inscription" type="submit" name="button" href="profile.php">Se connecter</button></a>
                     </div>
                     
-                    <a class="lien" href="./mdp.php">
-                    <p>Mot de passe oublié ?</p>
-                    </a>
+                    <a class="lien" href="./mdp.php">Mot de passe oublié ?</a>
 
                 </form>
             </div>
@@ -38,54 +36,55 @@
             <h2>Pas encore de compte ?</h2>
             <h3>Rejoignez la communauté dès aujourd’hui !</h3>
             <div class="inscription">
+            <form>
                 <div class="champ">
 
                     <div class="Prénom">
                     </div>
-                    <input type="text" name="Prénom" id="Prénom" placeholder="Prénom">
+                    <input class="input-profil" type="text" name="Prénom" id="Prénom" placeholder="Votre Prénom ..." required>
 
                     <div class="Nom">
                     </div>
-                    <input type="text" name="Nom" id="Nom" placeholder="Nom">
+                    <input class="input-profil" type="text" name="Nom" id="Nom" placeholder="Votre Nom ..." required>
                     <br>
                     <div class="E-mail">
                     </div>
-                    <input type="email" name="E-mail" id="E-mail" placeholder="E-mail">
-                    <br>
-
-                    <div class="Mot de passe">
-                    </div>
-                    <input type="text" name="Mot de passe" id="mdp" placeholder="Mot de passe">
+                    <input class="input-profil" type="email" name="E-mail" id="E-mail" placeholder="Votre E-mail ..." required>
                     <br>
                     <div class="Mot de passe">
                     </div>
-                    <input type="text" name="Mot de passe" id="confirmmdp" placeholder="Confirmez votre mot de passe">
+                    <input class="input-profil" type="password" name="Mot de passe" id="mdp" minlength="8" placeholder="Votre Mot de passe ..." required>
+                    <br>
+                    <div class="Mot de passe">
+                    </div>
+                    <input class="input-profil" type="password" name="Mot de passe" id="confirmmdp" minlength="8" placeholder="Confirmez votre mot de passe ..." required>
                     <br>
                     <div class="date">
                     </div>
-                    <input type="text" name="date de naissance" id="date" placeholder="Date de naissance (DD/MM/YYYY)">
+                    <input class="input-profil" type="date" name="date de naissance" id="date" placeholder="" required>
                     <br>
-
+                    </form>
                     <div class="check">
                         <input type="checkbox" id="CGU" name="CGU" required>
-                        <label for="CGU">J’ai lu et accepte les C.G.U.</label>
+                        <label for="CGU">J’ai lu et accepte les <a class="cgulink" href="C.G.U.php" >C.G.U</a></label>
                     </div>
                     <button name="button" type="submit">S'inscrire</button>
                 </div>
-                <div>
+            
+                <div class="right-text-inscription">
                     <div class="allignement">
                         <img src="front/assets/image/icones/star.svg" alt="star icon">
-                        <h4>Accédez à un contenu exclusif</h4>
+                        <h4 class="inscription">Accédez à un contenu exclusif</h4>
                     </div>
 
                     <div class="allignement">
                         <img src="front/assets/image/icones/speech-bubble.svg" alt="email icon">
-                        <h4>Donnez votre avis</h4>
+                        <h4 class="inscription">Donnez votre avis</h4>
                     </div>
 
                     <div class="allignement">
                         <img src="front/assets/image/icones/like.svg" alt="like icon">
-                        <h4>Sauvegardez vos articles favoris</h4>
+                        <h4 class="inscription">Sauvegardez vos articles favoris</h4>
                     </div>
                 </div>
 
