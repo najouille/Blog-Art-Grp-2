87ddcfcea666f4fc55655cd131e72f86826241a6
Mattèo Gauthier <matteo.gauthier@gmail.com>
2021-03-03 19:07:33 +0100

    error log

--- util/utilErrOn.php
+++ util/utilErrOn.php
@@ -10,4 +10,4 @@ ini_set('display_errors', 'on');
 ini_set('display_startup_errors', 'on');
 error_reporting(E_ALL);
 error_reporting(-1); // reports all errors
-ini_set("error_log", "/tmp/php-error.log");
+ini_set("error_log", "./php-error.log");
