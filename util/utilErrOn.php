fd671d00117d348a21bbde720bc25c92128b29d8
Mattèo Gauthier <matteo.gauthier@gmail.com>
2021-03-03 19:03:39 +0100

    update util

--- util/utilErrOn.php
+++ util/utilErrOn.php
@@ -6,6 +6,8 @@
 //
 //////////////////////////////////////////////
 //
-	ini_set('display_errors','on');
-	ini_set('display_startup_errors','on');
-	error_reporting(E_ALL);
+ini_set('display_errors', 'on');
+ini_set('display_startup_errors', 'on');
+error_reporting(E_ALL);
+error_reporting(-1); // reports all errors
+ini_set("error_log", "/tmp/php-error.log");
